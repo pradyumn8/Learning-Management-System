141dbb984147edfa639b261144c9228c0b23d6fd
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-12-22 12:10:47 +0530

    Basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+    <Button className='bg-green-400'>Check</Button>
+    </>
+  )
+}
+
+export default App
