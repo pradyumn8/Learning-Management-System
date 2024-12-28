14ab7e4248c172c00d16e0c6cb02259759609659
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-12-28 12:04:02 +0530

    basic UI for add lecture & course settings

--- client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
@@ -1,12 +1,63 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
+import { courseLandingInitialFormData } from '@/config';
+import { InstructorContext } from '@/context/instructor-context';
+import React, { useContext } from 'react';
 
+function CourseCurriculum() { 
+  const { courseCurriculumFormData, setCourseCurriculumFormData } = useContext(InstructorContext);
 
+  function handleNewLecture(){
+    setCourseCurriculumFormData([
+      ...courseCurriculumFormData,
+      {
+        ...courseLandingInitialFormData[0]
+      }
+    ])
+  }
+  console.log(courseCurriculumFormData);
 
-import React from 'react'
-
-function CourseCurriculum() {
   return (
-    <div>CourseCurriculum</div>
-  )
+    <Card>
+      <CardHeader>
+        <CardTitle>Create Course Curriculum</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Button
+          onClick={handleNewLecture}
+        >
+          Add Lecture
+        </Button>
+        <div className="mt-4 space-y-4">
+          {courseCurriculumFormData?.map((curriculumItem, index) => (
+            <div key={index} className="border p-5 rounded-md">
+              <div className='flex gap-5 items-center'>
+              <h3 className="font-semibold">Lecture {index + 1}</h3>
+              <Input
+              name={`title-${index+1}`}
+              placeholder="Enter lecture title"
+              className='max-w-96'
+              />
+              <div className="flex items-center space-x-2">
+                <Switch
+                checked={true}
+                id={`freePreview-${index+1}`}
+                />
+                <Label htmlFor={`freePreview-${index+1}`}>Free Preview</Label>
+              </div>
+                <div className='mt-6'>
+                  <Input type="file" accept="video/*" className="mb-4"/>
+                </div>
+            </div>
+            </div>  
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default CourseCurriculum
+export default CourseCurriculum;
